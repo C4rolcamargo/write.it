00f87f0b8620920cabdf739869409786445b1839
Calorithefacto <125594287+VictorCalori@users.noreply.github.com>
2024-05-28 15:27:25 -0300

    Update App.js

--- Loginreact/App.js
+++ Loginreact/App.js
@@ -1,24 +1,27 @@
 // App.js
 import React from 'react';
-import { SafeAreaView, StyleSheet } from 'react-native';
-import Login from './Login';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-const App = () => {
+import LoginScreen from './Login.js';
+import CadastroScreen from './Cadastro.js';
+import PerfilScreen from './Perfil.js';
+import RecuperarScreen from './RecuperarSenha.js';
+
+const Stack = createStackNavigator();
+
+export default function App() {
   return (
-    <SafeAreaView style={styles.container}>
-      <Login />
-    </SafeAreaView>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen name="Login" component={LoginScreen} />
+        <Stack.Screen name="Cadastro" component={CadastroScreen} />
+        <Stack.Screen name="Perfil" component={PerfilScreen} />
+        <Stack.Screen name="RecuperarSenha" component={RecuperarScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-});
-
-export default App;
-
+}
 
 
 
